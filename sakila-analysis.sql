9443bb82ff634ed0692072e8136575f107f062d2
BhartiMoudgil <bharti.forwork@gmail.com>
2024-07-20 13:35:01 +0530

    Update sakila-analysis.sql

--- sakila-analysis.sql
+++ sakila-analysis.sql
@@ -1,9 +1,3 @@
-/***************************************************/
-/* SQL for Data Analysis - Weekender Crash Course  */
-/***************************************************/
-
-/*Before you run these queries, make sure to create Sakila DB and import the related data first*/
-
 /* Email Campaigns for customers of Store 2
 First, Last name and Email address of customers from Store 2*/
 SELECT first_name, last_name,email
